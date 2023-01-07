97fa052433ee5b9afd9e1806a2b5a1c743d86524
uktamov1 <uktamovvburhon0409@gmail.com>
2023-01-07 20:04:04 +0500

    a+b main  project

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(sum(5,5));
+    }
+    public static int sum (int a,int b){
+        return a+b;
+    }
+}
